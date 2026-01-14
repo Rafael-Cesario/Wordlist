55142fb02c988fd3455c6345d32c5ea92b07ece8
Rafael <rafaelOliveiracds@gmail.com>
2026-01-14 14:32:52 -0300

    User state, change value

--- Frontend/src/app/components/TextInput.tsx
+++ Frontend/src/app/components/TextInput.tsx
@@ -3,16 +3,27 @@ interface TextInputProps {
     title: string;
     fieldName: string;
     error: string | undefined;
+    value: string;
+    changeValue: (newValue: string) => void;
   };
 }
 
-export const TextInput = ({ props: { title, error, fieldName } }: TextInputProps) => {
+export const TextInput = ({ props: { title, error, fieldName, value, changeValue } }: TextInputProps) => {
   return (
     <div className="flex flex-col mb-4 w-100">
       <label className="ml-4 text-sm text-neutral-400" htmlFor={fieldName}>
         {title}
       </label>
-      <input className="bg-faded px-4 py-2 rounded-xs mt-1" type="text" id={fieldName} placeholder={title} />
+
+      <input
+        value={value}
+        className="bg-faded px-4 py-2 rounded-xs mt-1"
+        type="text"
+        id={fieldName}
+        placeholder={title}
+        onChange={(e) => changeValue(e.target.value)}
+      />
+
       <p className="text-red px-4 mt-1">{error}</p>
     </div>
   );
--- Frontend/src/app/page.tsx
+++ Frontend/src/app/page.tsx
@@ -1,6 +1,25 @@
+"use client";
+
+import { useState } from "react";
 import { TextInput } from "./components/TextInput";
+import { produce } from "immer";
 
 export default function Home() {
+  const defaultUser = { email: "", name: "", password: "", passwordCheck: "" };
+  const [user, setUser] = useState(defaultUser);
+
+  const createUser = () => {
+    console.log({ user });
+  };
+
+  const changeValue = (field: keyof typeof defaultUser, newValue: string) => {
+    const newState = produce(user, (draft) => {
+      draft[field] = newValue;
+    });
+
+    setUser(newState);
+  };
+
   return (
     <>
       <header className="flex justify-end p-5">
@@ -11,12 +30,45 @@ export default function Home() {
         <h1 className="text-center text-3xl font-bold">Criar Conta</h1>
         <p className="text-center text-neutral-400">Preencha os dados abaixo para criar sua conta</p>
 
-        <form className="flex flex-col items-center mt-8">
-          <TextInput props={{ title: "Email", fieldName: "email", error: "Este email já esta em uso" }} />
-          <TextInput props={{ title: "Nome", fieldName: "name", error: "Este email já esta em uso" }} />
-          <TextInput props={{ title: "Senha", fieldName: "password", error: "Este email já esta em uso" }} />
+        <form onSubmit={(e) => (e.preventDefault(), createUser())} className="flex flex-col items-center mt-8">
           <TextInput
-            props={{ title: "Confirmar Senha", fieldName: "passwordCheck", error: "Este email já esta em uso" }}
+            props={{
+              title: "Email",
+              fieldName: "email",
+              error: "Este email já esta em uso",
+              value: user.email,
+              changeValue: (value) => changeValue("email", value),
+            }}
+          />
+
+          <TextInput
+            props={{
+              title: "Nome",
+              fieldName: "name",
+              error: "Este email já esta em uso",
+              value: user.name,
+              changeValue: (value) => changeValue("name", value),
+            }}
+          />
+
+          <TextInput
+            props={{
+              title: "Senha",
+              fieldName: "password",
+              error: "Este email já esta em uso",
+              value: user.password,
+              changeValue: (value) => changeValue("password", value),
+            }}
+          />
+
+          <TextInput
+            props={{
+              title: "Confirmar Senha",
+              fieldName: "passwordCheck",
+              error: "Este email já esta em uso",
+              value: user.passwordCheck,
+              changeValue: (value) => changeValue("passwordCheck", value),
+            }}
           />
 
           <button className="bg-royal-blue w-100 rounded-xs px-4 py-2 mt-4">Confirmar</button>
